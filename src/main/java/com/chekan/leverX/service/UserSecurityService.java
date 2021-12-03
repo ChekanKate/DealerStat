3c65a39b9303db33851568703bfde3146f540d81
Chekan Kateryna <chekankate@gmail.com>
2021-12-03 15:54:58 +0200

    Spring Security config

---
+++ src/main/java/com/chekan/leverX/service/UserSecurityService.java
@@ -0,0 +1,70 @@
+package com.chekan.leverX.service;
+
+import com.chekan.leverX.dao.UserDAO;
+import com.chekan.leverX.entity.Role;
+import com.chekan.leverX.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+//
+//    private UserDAO userDAO;
+//
+//    @Autowired
+//    public void setUserRepository(UserDAO userDAO) {
+//        this.userDAO = userDAO;
+//    }
+//
+//    public User findByUsername(String username) {
+//        return userDAO.getByUserEmail(username);
+//    }
+//
+//    @Override
+//    @Transactional
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User user = findByUsername(username);
+//        if(user == null) {
+//            throw new UsernameNotFoundException(String.format("User '%s' not found", username));
+//        }
+//        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+//                mapRolesToAuthorities(user.getRoles()));
+//    }
+//
+//
+//    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+//        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
+//    }
+
+    private UserDAO userDAO;
+
+
+    public UserSecurityService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDAO.getByUserEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("User '%s' not found", username));
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                mapRolesToAuthorities(user.getRoles()));
+    }
+
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
+    }
+}
