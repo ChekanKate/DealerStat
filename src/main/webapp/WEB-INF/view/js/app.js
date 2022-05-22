83d8cfee1abbed0efd98bfeaa132d4efb1c505aa
Kiryl Ivanou <kiryl.ivanou@sap.com>
2022-05-22 22:04:22 +0200

    add play sound functionality

--- src/main/webapp/WEB-INF/view/js/app.js
+++ src/main/webapp/WEB-INF/view/js/app.js
@@ -3,8 +3,26 @@ const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
 
 navBtn.addEventListener("click", () => {
-	navbar.classList.add("showNav");
+    navbar.classList.add("showNav");
 });
 navClose.addEventListener("click", () => {
-	navbar.classList.remove("showNav");
+    navbar.classList.remove("showNav");
 });
+
+// Play Sound on click
+const button = document.querySelector("#to_games_btn");
+
+button.addEventListener("click", () => {
+    playSound("sound/click.mp3");
+});
+
+function playSound(url) {
+    var audio = document.createElement("audio");
+    audio.style.display = "none";
+    audio.src = url;
+    audio.autoplay = true;
+    audio.onended = function () {
+        audio.remove(); //Remove when played.
+    };
+    document.body.appendChild(audio);
+}
